2f43f3a1655122e500d37934c6100208afc9bd8f
chebur5581 <110908198+chebur5581@users.noreply.github.com>
2022-08-17 09:20:31 +0300

    added different type of engine with 3D support

---
+++ CBEngine[3DSupport]/object.py
@@ -0,0 +1,35 @@
+import pygame as pg
+from numpy import array
+import math
+
+
+class Object():  # класс для создания объектов.
+    def __init__(self, vertex, facets, pos, screen):
+        self.vertex = vertex  # вершины
+        self.facets = facets  # грани
+        self.x, self.y, = pos  # позиция
+
+        self.basis = array([(1, 0),  # базисы
+                            (0, 1)])
+
+        self.screen = screen  # экран на котором происходит отрисовка
+
+    def render(self):  # обработка граней и добавление в массив точек которые нужно объединить в полигон
+        for facet in self.facets:
+            sub_poly = []
+            for ind in facet:
+                sub_poly.append(self.vertex[ind])
+            self.draw_poly(sub_poly)  # вызов функции отрисовки полигона
+
+    def draw_poly(self, vertex):  # отрисовка полигона исходя из переданных ему вершин
+        poly = []
+        for vert in enumerate(vertex):
+            poly.append(vert[1][0] * self.basis[0] + vert[1][1] * self.basis[1])
+            # ^вычисление положения точки/вектора иcходя из базисов^
+            poly[vert[0]][0] += self.x  # смещение
+            poly[vert[0]][1] += self.y
+        pg.draw.polygon(self.screen, (150, 200, 150), poly, 2)  # отрисовка полигона
+
+    def rotate_z(self, angle):  # поворот вокруг оси z или в плоскости x y
+        self.basis = array([(math.cos(angle), -math.sin(angle)),
+                            (math.sin(angle), math.cos(angle))])
---
+++ CBEngine[3DSupport]/world.py
@@ -0,0 +1,37 @@
+from numpy import array
+from object import Object
+
+
+class World():  # класс всех объектов на экране
+    def __init__(self, screen):
+        self.screen = screen  # передача экрана для отрисовки
+
+        self.create_objects()  # вызов функции создания объектов
+
+    def create_objects(self):  # создание объектов с вершинами
+        self.obj = self.get_object_from_file('monkey_test.obj', 200, (300, 400))
+
+    # функция квадрата
+    def quad(self, pos, size=float or int, faces=array([((0, 1, 2)), ((0, 2, 3))])):
+        vert = array([(0, 0), (size, 0), (size, size), (0, size)])
+        return Object(vert, faces, pos, self.screen)
+
+    # функция с произвольными вершинами и гранями
+    def figure(self, pos, vertex, facets):
+        return Object(vertex, facets, pos, self.screen)
+
+    def render_world(self):  # отрисовка всех объектов
+        self.obj.render()
+
+    # импорт объекта из obj файла
+    # коментов побольше чем обычно потому что код функции не совсем мой и таким обычно не пользуюсь
+    def get_object_from_file(self, filename, size, pos):
+        vertex, faces = [], []
+        with open(filename) as f:
+            for line in f:
+                if line.startswith('v '):  # если линия начинается с символа v
+                    vertex.append([float(i) * size for i in line.split()[1:]] + [1])  # добавление вершин
+                elif line.startswith('f'):  # если линия начинается с символа f
+                    faces_ = line.split()[1:]
+                    faces.append([int(face_.split('/')[0]) - 1 for face_ in faces_])  # добавление граней
+        return Object(vertex, faces, pos, self.screen)  # возврат объекта с вершинами и гранями из файла
